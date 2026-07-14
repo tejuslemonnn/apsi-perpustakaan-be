cb51dee2e2ef925831199546f5fcdb3781e6aeeb
vitoramadhan <vito@itpi.co.id>
2026-07-14 17:36:11 +0700

    fix

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    'allowed_origins' => [
+    'https://apsi-perpustakaan-fe-production.up.railway.app',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    ],
 
     'allowed_origins_patterns' => [],
 
